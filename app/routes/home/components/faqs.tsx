6b3aacbfdae412d2fcbbe3a0300c54f8ce338dd9
TienDat <multivncraft@gmail.com>
2025-11-05 21:59:00 +0700

    dev : add gallery blocks

--- app/routes/home/components/faqs.tsx
+++ app/routes/home/components/faqs.tsx
@@ -4,11 +4,10 @@ import {
   AccordionItem,
   AccordionPanel,
 } from "~/components/ui/accordion";
-
-import { TimelineContent } from "~/components/timeline-animation";
-import VerticalCutReveal from "~/components/vertical-cut-reveal";
-import { Plus } from "lucide-react";
+import { Play } from "lucide-react";
 import { useRef } from "react";
+import  { TimelineContent } from "~/components/timeline-animation";
+import VerticalCutReveal from "~/components/vertical-cut-reveal";
 
 type FAQItem = {
   question: string;
@@ -17,48 +16,33 @@ type FAQItem = {
 
 const faqData: FAQItem[] = [
   {
-    question: "What services does your AI agency offer?",
+    question: "How we will keep your data safe , secure and well maintance?",
     answer:
       "We specialize in custom AI solutions including chatbots, predictive analytics, computer vision, NLP, and automation workflows tailored to your business needs.",
   },
   {
-    question: "Can you integrate AI into our existing systems?",
+    question: "How we will keep your data safe , secure and well maintance?",
     answer:
       "Absolutely! We offer seamless integration with CRMs, ERPs, databases, APIs, and other third-party tools your business relies on.",
   },
   {
-    question: "What if the AI model doesn't perform well?",
+    question: "How we will keep your data safe , secure and well maintance?",
     answer:
       "We conduct thorough testing and offer ongoing optimization post-launch. If something's off, we'll tweak it until it delivers results.",
   },
   {
-    question: "Do you provide documentation and training?",
+    question: "How we will keep your data safe , secure and well maintance?",
     answer:
       "Yes, we provide clear documentation and offer team training to help you and your staff understand and make the most of the AI systems we implement.",
   },
   {
-    question: "What's your pricing model?",
+    question: "How we will keep your data safe , secure and well maintance?",
     answer:
-      "We offer flexible pricing based on project complexity and duration—ranging from fixed project fees to retainer and hourly models.",
-  },
-  {
-    question: "How do we get started?",
-    answer:
-      "Just reach out via our contact form or email. We'll schedule a free consultation to understand your needs and propose the best AI strategy for you.",
-  },
-  {
-    question: "Do you offer support post-deployment?",
-    answer:
-      "Yes, our partnership doesn't end after delivery. We offer maintenance plans, bug fixes, performance monitoring, and continuous updates.",
-  },
-  {
-    question: "Can we see previous case studies or demos?",
-    answer:
-      "Definitely! We're happy to share relevant case studies and showcase live demos of our previous AI solutions, depending on NDA limitations.",
+      "Yes, we provide clear documentation and offer team training to help you and your staff understand and make the most of the AI systems we implement.",
   },
 ];
 
-export default function Faqs1() {
+export default function Faqs3() {
   const faqsRef = useRef<HTMLDivElement>(null);
   const revealVariants = {
     visible: (i: number) => ({
@@ -72,92 +56,126 @@ export default function Faqs1() {
     }),
     hidden: {
       filter: "blur(10px)",
-      y: 0,
+      y: -20,
       opacity: 0,
     },
   };
 
   return (
     <div
-      className="p-5 mx-auto rounded-2xl max-w-xl shadow-sm mt-4"
+      className="sm:p-10 p-5 mx-auto w-full max-w-7xl shadow-sm grid md:grid-cols-2 gap-5"
       ref={faqsRef}
     >
-      <article className="max-w-2xl mx-auto text-center space-y-3 pb-10 pt-4">
+      <div className="flex flex-col justify-between">
+        <div>
+          <TimelineContent
+            as="span"
+            animationNum={0}
+            timelineRef={faqsRef}
+            customVariants={revealVariants}
+            className="text-sm  border p-1 px-2 bg-blue-600 text-white rounded-md font-normal"
+          >
+            UI-Layout Sass analytics
+          </TimelineContent>
+          <h1 className="lg:text-6xl md:text-5xl text-4xl text-black pt-4">
+            <VerticalCutReveal
+              splitBy="words"
+              staggerDuration={0.15}
+              staggerFrom="first"
+              reverse={true}
+              containerClassName="justify-start"
+              transition={{
+                type: "spring",
+                stiffness: 250,
+                damping: 40,
+                delay: 0.4,
+              }}
+            >
+              Frequently asked questions
+            </VerticalCutReveal>
+          </h1>
+        </div>
+
         <TimelineContent
-          as="span"
-          animationNum={0}
+          as="div"
+          animationNum={1}
           timelineRef={faqsRef}
           customVariants={revealVariants}
-          className="text-sm font-semibold"
+          className="border border-neutral-200 p-4 rounded-lg bg-gradient-to-t text-white from-blue-50 to-blue-700 md:block hidden"
         >
-          TRUSTED BY
+          <h2 className="text-2xl font-semibold">Still have questions?</h2>
+          <p className="text-sm py-2">
+            Can't find the answer to your question? Send us an email and we'll
+            get back to you as soon as possible!
+          </p>
+          <button className="active:shadow-none bg-gradient-to-t from-blue-500 to-blue-600 shadow-lg shadow-blue-600 text-white p-2 px-3 mt-5 rounded-md">
+            Send email
+          </button>
         </TimelineContent>
-
-        <h2 className="sm:text-5xl text-4xl font-medium">
-          <VerticalCutReveal
-            splitBy="words"
-            staggerDuration={0.15}
-            staggerFrom="first"
-            reverse={true}
-            containerClassName="justify-center"
-            transition={{
-              type: "spring",
-              stiffness: 250,
-              damping: 40,
-              delay: 0.4, // After "TRUSTED BY" (animationNum 0)
-            }}
-          >
-            Frequently Asked Questions
-          </VerticalCutReveal>
-        </h2>
-      </article>
-
-      <TimelineContent
-        as="div"
-        animationNum={1}
-        timelineRef={faqsRef}
-        customVariants={revealVariants}
-      >
-        <Accordion defaultValue="item-2">
-          {faqData.map((item, index) => (
-            <AccordionItem
-              key={index}
-              value={`item-${index}`}
-              className="mb-0 rounded-xl bg-neutral-100 border border-neutral-200 w-full data-[active]:pb-3 mt-3"
-            >
-              <TimelineContent
-                as="div"
-                animationNum={2 + index}
-                timelineRef={faqsRef}
-                customVariants={revealVariants}
+      </div>
+      <div>
+        <div className="mt-3">
+          <Accordion defaultValue="item-2">
+            {faqData.map((item, index) => (
+              <AccordionItem
+                key={index}
+                value={`item-${index}`}
+                className="mb-0 bg-gray-100 border p-4 rounded-xl border-gray-200 w-full py-2 mt-4"
               >
-                <AccordionHeader
-                  customIcon
-                  className="hover:no-underline p-0 pl-4 py-1 relative data-[active]:bg-transparent hover:bg-transparent text-neutral-950 hover:text-neutral-950  data-[active]:text-neutral-950 sm:text-base text-sm"
+                <TimelineContent
+                  as="div"
+                  animationNum={2 + index}
+                  timelineRef={faqsRef}
+                  customVariants={revealVariants}
                 >
-                  <span className="font-medium">{item.question}</span>
-                  <span className="relative group-data-[active]:rotate-90 border border-neutral-200 bg-white text-neutral-950 p-2 -translate-x-1 rounded-xl">
-                    <Plus className="group-data-[active]:rotate-90 transition-all duration-300" />
-                  </span>
-                </AccordionHeader>
-              </TimelineContent>
-              <TimelineContent
-                as="div"
-                animationNum={2 + index}
-                timelineRef={faqsRef}
-                customVariants={revealVariants}
-              >
-                <AccordionPanel
-                  className="space-y-4 w-full mx-auto  bg-neutral-100"
-                  articleClassName="pt-3 px-2 bg-neutral-100"
+                  <AccordionHeader
+                    customIcon
+                    className="hover:no-underline p-0  py-2 relative data-[active]:bg-transparent hover:bg-transparent text-black  sm:text-base text-sm"
+                  >
+                    <span className="font-medium lg:text-lg">
+                      {item.question}
+                    </span>
+                    <span className="relative bg-white p-3 -translate-x-1 rounded-xl border border-neutral-200">
+                      <Play className="w-5 h-5 fill-blue-600 text-blue-600 group-data-[active]:rotate-90 -rotate-90 transition-all duration-300" />
+                    </span>
+                  </AccordionHeader>
+                </TimelineContent>
+                <TimelineContent
+                  as="div"
+                  animationNum={2 + index}
+                  timelineRef={faqsRef}
+                  customVariants={revealVariants}
                 >
-                  <p className="text-sm sm:text-base">{item.answer}</p>
-                </AccordionPanel>
-              </TimelineContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </TimelineContent>
+                  <AccordionPanel
+                    className="space-y-4 w-full mx-auto bg-neutral-100 px-0"
+                    articleClassName="pt-2 px-0 bg-neutral-100"
+                  >
+                    <p className="text-sm lg:text-base text-neutral-700">
+                      {item.answer}
+                    </p>
+                  </AccordionPanel>
+                </TimelineContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
+        <TimelineContent
+          as="div"
+          animationNum={1}
+          timelineRef={faqsRef}
+          customVariants={revealVariants}
+          className="border border-neutral-200 p-4 rounded-lg bg-gradient-to-t text-white from-blue-50 to-blue-700 md:hidden block mt-10"
+        >
+          <h2 className="text-2xl font-semibold">Still have questions?</h2>
+          <p className="text-sm py-2">
+            Can't find the answer to your question? Send us an email and we'll
+            get back to you as soon as possible!
+          </p>
+          <button className="active:shadow-none bg-gradient-to-t from-blue-500 to-blue-600 shadow-lg shadow-blue-600 text-white p-2 px-3 mt-5 rounded-md">
+            Send email
+          </button>
+        </TimelineContent>
+      </div>
     </div>
   );
 }
--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -2,9 +2,9 @@ import type { Route } from "./+types/home";
 import About from "./components/about";
 import FAQs from "./components/faqs";
 import { Footer } from "./components/footer";
+import { Gallery6 } from "./components/gallery";
 import AgencyHero from "./components/hero";
 import ClientFeedback from "./components/testimonial";
-
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -13,10 +13,12 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  
   return (
     <main className="flex flex-col gap-4" >
       <AgencyHero />
       <ClientFeedback />  
+      <Gallery6 />
       <About />
       <FAQs />
       <Footer/>
